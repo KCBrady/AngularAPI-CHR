4e3cf79759a22c9dc98e1b10aa5f524125c61759
KCBrady <kieran.caulfield@bradyplc.com>
2022-05-19 16:51:44 +0100

    Angular API connected to API connected to SQL Server locally

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  @ViewChild("agGrid",{ static: false}) agGrid!:AgGridAngular;
+  title = 'my-app';
+
+ /* columnDefs = [
+    { headerName: "Country", field: "country", sortable: true, filter: true },
+    { headerName: "Name", field: "name", sortable: true, filter: true, checkboxSelection: true},
+    { headerName: "Web", field: "web_pages", sortable: true, filter: true}
+  ];*/
+  columnDefs = [
+    { headerName: "Country", field: "country", rowGroup: true },
+    { headerName: "Name", field: "name", sortable: true, filter: true, checkboxSelection: true},
+    { headerName: "Web", field: "web_pages", sortable: true, filter: true}
+  ];
+
+  autoGroupColumnDef = {
+    headerName: 'Country',
+    field: 'country',
+    callRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true
+    }
+  }
+
+  rowData: any;
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit() {
+    //this.rowData = this.http.get("https://api.myjson.com/bins/15psn9");
+    this.rowData = this.http.get("http://universities.hipolabs.com/search?country=");
+  }
+
+  getSelectedRows() {
+    const selectedNodes = this.agGrid.api.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectDataStringPresentation = selectedData.map(node => node.name + ' ' + node.web_pages).join(", ");
+    alert(selectDataStringPresentation);
+  }
+
+}
